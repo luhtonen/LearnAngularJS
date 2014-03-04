41323c7797f3374e22dd4cb39d8643907fc78e6f
Eduard Luhtonen <luhtonen@gmail.com>
2014-03-04 20:54:24 +0100

    refactoring JavaScript code to separate js file

---
+++ public_html/AngularJS-book/chapter02/js/chapter02.js
@@ -0,0 +1,9 @@
+var myAppModule = angular.module('myApp', []);
+
+myAppModule.controller('TextController',
+function($scope) {
+    var someText = {};
+    someText.message = 'You have started your journey';
+    $scope.someText = someText;
+    $scope.greeting = 'Hi there';
+});
