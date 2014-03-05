b48d9d5add3d6225d1445b96c186989bc4dbc391
Eduard Luhtonen <luhtonen@gmail.com>
2014-03-05 08:55:55 +0100

    using $watch() to watch for changes

--- public_html/AngularJS-book/chapter02/js/chapter02.js
+++ public_html/AngularJS-book/chapter02/js/chapter02.js
@@ -13,7 +13,9 @@ function($scope) {
 .controller('StartUpController', function($scope) {
     $scope.funding = { startingEstimate: 0 };
     
-    $scope.computeNeeded = function() {
+    computeNeeded = function() {
         $scope.funding.needed = $scope.funding.startingEstimate * 10;
     };
+    
+    $scope.$watch('funding.startingEstimate', computeNeeded);
 });
