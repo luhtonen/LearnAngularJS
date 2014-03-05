7f273d416c43c28f9a2124f4ead016f30a7bd594
Eduard Luhtonen <luhtonen@gmail.com>
2014-03-05 08:33:56 +0100

    form input added and some refactoring done

--- public_html/AngularJS-book/chapter02/js/chapter02.js
+++ public_html/AngularJS-book/chapter02/js/chapter02.js
@@ -6,4 +6,14 @@ function($scope) {
     someText.message = 'You have started your journey';
     $scope.someText = someText;
     $scope.greeting = 'Hi there';
+})
+.controller('SomeController', function($scope) {
+    $scope.youCheckedIt = true;
+})
+.controller('StartUpController', function($scope) {
+    $scope.funding = { startingEstimate: 0 };
+    
+    $scope.computeNeeded = function() {
+        $scope.funding.needed = $scope.funding.startingEstimate * 10;
+    };
 });
