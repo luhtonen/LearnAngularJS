63ebcd8bcef14c25d4d65a2307bf3588553957cc
Eduard Luhtonen <luhtonen@gmail.com>
2014-03-06 18:06:02 +0100

    CSS classes and styles

--- public_html/AngularJS-book/chapter02/js/repeats.js
+++ public_html/AngularJS-book/chapter02/js/repeats.js
@@ -20,17 +20,47 @@ repeatsAppModule.controller('StudentListController', function ($scope) {
     $scope.album = album;
 }).controller('DeathrayMenuController', function ($scope) {
     $scope.menuState = {show: false};
+    $scope.isDisabled = false;
     
     $scope.toggleMenu = function () {
         $scope.menuState.show = !$scope.menuState.show;
     };
     $scope.stun = function () {
+        $scope.isDisabled = 'true';
         $scope.actionResult = 'enemy is stunned';
     };
     $scope.disintegrate = function () {
+        $scope.isDisabled = false;
         $scope.actionResult = 'enemy was disintegrate';
     };
     $scope.erase = function () {
+        $scope.isDisabled = false;
         $scope.actionResult = 'enemy was erased from the history forever';
+    };
+}).controller('HeaderController', function($scope) {
+    $scope.messageText = 'Here is no errors';
+    $scope.isError = false;
+    $scope.isWarning = false;
+    $scope.showError = function() {
+        $scope.messageText = 'This is an error!';
+        $scope.isError = true;
+        $scope.isWarning = false;
+    };
+    $scope.showWarning = function() {
+        $scope.messageText = 'Just a warning. Please carry on.';
+        $scope.isError = false;
+        $scope.isWarning = true;
+    };
+    $scope.reset = function() {
+        $scope.messageText = 'No warnings, no errors';
+        $scope.isError = false;
+        $scope.isWarning = false;
+    };
+}).controller('RestaurantTableController', function($scope) {
+    $scope.directory = [{name: "The Handsome Heifer", cuisine: "BBQ"},
+                        {name: "Green's Green Greens", cuisine: "Salads"},
+                        {name: "House of Fine Fish", cuisine: "Seafood"}];
+    $scope.selectRestaurant = function(row) {
+        $scope.selectedRow = row;
     };
 });
