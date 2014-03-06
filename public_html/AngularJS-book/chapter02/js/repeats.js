0c77836ac4971b6fca14fb05f13007c43a78c467
Eduard Luhtonen <luhtonen@gmail.com>
2014-03-06 08:59:32 +0100

    using repeater's variables $index etc

--- public_html/AngularJS-book/chapter02/js/repeats.js
+++ public_html/AngularJS-book/chapter02/js/repeats.js
@@ -3,11 +3,18 @@ var repeatsAppModule = angular.module('repeatsApp', []);
 var students = [{name:'Mary Contrary', id:'1'}, 
                 {name:'Jack Sprat', id:'2'}, 
                 {name:'Jill Hill', id:'3'}];
-            
+
+var album = [{name:'Southwest Serenade', duration: '2:34'}, 
+              {name:'Northern Light Waltz', duration: '3:21'},
+              {name:'Eastern Tango', duration: '17:45'}];
+             
 repeatsAppModule.controller('StudentListController', function($scope) {
     $scope.students = students;
     
     $scope.insertTom = function() {
         $scope.students.splice(1, 0, {name: 'Tom Thumb', id: '4'});
     };
+})
+.controller('AlbumController', function($scope) {
+    $scope.album = album;
 });
