a8ccd9ebc8af8cac2903412c1436bdca0b3a77c2
Eduard Luhtonen <luhtonen@gmail.com>
2014-03-07 19:37:47 +0100

    optimizing witch even feature

--- public_html/AngularJS-book/chapter02/js/shopping.js
+++ public_html/AngularJS-book/chapter02/js/shopping.js
@@ -9,16 +9,13 @@ shoppingAppModule.controller('CartController', function($scope) {
         {title: 'Pebbles', quantity: 5, price: 6.95}
     ];
     
-    var calculateTotals = function() {
+    $scope.$watch(function() {
         var total = 0;
         for (var i = 0, len = $scope.items.length; i < len; i++) {
             total = total + $scope.items[i].price * $scope.items[i].quantity;
         }
         $scope.bill.totalCart = total;
-        $scope.bill.discount = 
         $scope.bill.discount = total > 100 ? 10 : 0;
         $scope.bill.subtotal = total - $scope.bill.discount;
-    };
-    
-    $scope.$watch('items', calculateTotals, true);
+    });
 });
