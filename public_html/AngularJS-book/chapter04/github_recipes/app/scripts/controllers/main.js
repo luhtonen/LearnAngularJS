732c1eb1eefabf3688d36aedc39143eecc58e986
Eduard Luhtonen <luhtonen@gmail.com>
2014-06-17 12:52:49 +0200

    AngularJS books chapter04 examples

---
+++ public_html/AngularJS-book/chapter04/github_recipes/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('githubRecipesApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
